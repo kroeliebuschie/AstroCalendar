81e9bfe45b6b863381efb36f98bc195c3f853ad7
Rohit Sardessai <rohit.sardessai@gmail.com>
2022-12-11 21:20:47 +0530

    Updated for Python 3.
    Updated the script to make it compatible with python 3. Bumped urllib version from urllib2 to urllib3.

--- getAstroCal.py
+++ getAstroCal.py
@@ -9,7 +9,7 @@
 ##
 ## Author: Mehdi Nellen, Tuebingen 2015
 
-import urllib2
+import urllib3
 import re
 from bs4 import BeautifulSoup
 from icalendar import *
@@ -28,9 +28,9 @@ months = {'January': 1,
           'October': 10,
           'November': 11,
           'December': 12}
-# specify the range of years you would like to have
+# specify the range of years you would like to have. SeaSky has the calendars up tp 2030 at this time.
 # but dont forget to add 1 to the last year
-years = [2018, 2034]
+years = [2022, 2031]
 ical_f='astroCal.ics'
 
 def getDate(li, year):
@@ -39,7 +39,7 @@ def getDate(li, year):
        it may also return multiple dates
        depending on the content'''
     # print remove commas, split the string to only get the month and day in a list
-    splitD = filter(None, re.sub(",", "", li.p.text).split("-")[0].split(" "))
+    splitD = list(filter(None, re.sub(",", "", li.p.text).split("-")[0].split(" ")))
     # save the date(s) into a list
     result_date   = [ date(year, months[splitD[0]], int(day)) for day in splitD[1:] ]
     return result_date
@@ -59,7 +59,7 @@ def getDescr(li):
 
 def saveIcal(cal):
     ''' Save the cal into a file '''
-    ical_handle = open(ical_f, 'w')
+    ical_handle = open(ical_f, 'wb')
     ical_handle.write(cal.to_ical())
     ical_handle.close()
     print('saved as %s'%ical_f)
@@ -91,7 +91,7 @@ def extractTime(description, date):
 
 def addEvents(page, year, cal):
     ''' adds events to the calendar '''
-    soup  = BeautifulSoup(page)
+    soup  = BeautifulSoup(page, features="lxml")
     table = soup.find_all("div", {"id": "right-column-content"})[1]
 
     # loop over the list and get all list entries
@@ -134,8 +134,10 @@ def main():
     # loop over the years
     for year in range(*years):
         URL = "http://www.seasky.org/astronomy/astronomy-calendar-%s.html"%year
-        print URL
-        page  = urllib2.urlopen(URL)
+        print(URL)
+        http = urllib3.PoolManager()
+        r = http.request('GET', URL)
+        page  = r.data
         cal = addEvents(page, year, cal)
     saveIcal(cal)
 
